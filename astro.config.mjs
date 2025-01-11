2d12a76731d9ab9c666ec33ee54d8149e646de66
yangyang <charlesmaxwellyoung@gmail.com>
2025-01-12 00:57:35 +0800

    feat: 新增 博客使用 view-transition-api 添加黑暗模式

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// @ts-nocheck
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 import vercel from "@astrojs/vercel";
+import remarkMermaid from "remark-mermaidjs";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: {
-    defaultStrategy: 'viewport'
+  site: "https://www.ethyoung.me",
+  markdown: {
+    remarkPlugins: [remarkMermaid],
   },
-  site: 'https://www.ethyoung.me',
-  integrations: [ react(), tailwind(), mdx(), sitemap()],
+  integrations: [expressiveCode(), react(), tailwind(), mdx(), sitemap()],
   output: "static",
   adapter: vercel({
     webAnalytics: {
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,7 @@ import twTypography from "@tailwindcss/typography";
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./data/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   daisyui: {
