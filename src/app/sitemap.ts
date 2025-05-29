40014c4e69bce103067c8954d42d30f9d8e61e29
yangyang <charlesmaxwellyoung@gmail.com>
2025-05-29 10:19:07 +0800

    feat: 更新 sitemap 逻辑，使用 NAV_LIST 动态生成路由

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 import type { MetadataRoute } from "next";
 
-import { SITE } from "@/config";
+import { NAV_LIST, SITE } from "@/config";
 import { allPosts } from "@/lib/mdx";
 
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -9,8 +9,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     url: `${SITE.url}/${post.slug}`,
     lastModified: dayjs(post.metadata.updatedAt).toISOString(),
   }));
-
-  const routes = ["", "/blogs", "/notes", "/links"].map((route) => ({
+  const urls = NAV_LIST.map((item) => item.url);
+  const routes = ["", ...urls].map((route) => ({
     url: `${SITE.url}${route}`,
     lastModified: dayjs().toISOString(),
   }));
