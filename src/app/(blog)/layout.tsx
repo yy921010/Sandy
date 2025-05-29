1d735120ddab3aa9f63a2ba65040b008a35b04ea
yangyang <charlesmaxwellyoung@gmail.com>
2025-05-29 23:06:56 +0800

    feat: 更新布局和目录组件，添加按钮组件以增强可访问性和交互性

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="container mx-auto py-6 px-4 flex-1 max-w-4xl">
+      <main className="container mx-auto py-6 px-4 flex-1 max-w-4xl group/toc">
         {children}
       </main>
       <Footer />
--- src/components/toc.tsx
+++ src/components/toc.tsx
@@ -1,6 +1,7 @@
 import type { Heading } from "@/types/blog";
 import { TableOfContents } from "lucide-react";
 import { cn, generateNestedTOC } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 export const Toc = ({ toc }: { toc: Heading[] }) => {
   const renderToc = generateNestedTOC(toc);
@@ -9,12 +10,9 @@ export const Toc = ({ toc }: { toc: Heading[] }) => {
       <ul className="text-ellipsis list-none m-0 gap-1 p-0">
         {items.map((item) => (
           <li className="m-0" key={item.text}>
-            <a
-              className="link link-hover transition-all"
-              href={`#${item.slug}`}
-            >
-              {item.text}
-            </a>
+            <Button asChild size="sm" variant="link">
+              <a href={`#${item.slug}`}> {item.text}</a>
+            </Button>
             {item.children &&
               item.children.length > 0 &&
               renderTOC(item.children)}
@@ -24,15 +22,15 @@ export const Toc = ({ toc }: { toc: Heading[] }) => {
     );
   }
   return (
-    <div className="m-auto fixed top-24 left-5 pb-3 z-50 overflow-hidden text-sm hidden h-3/4 flex-col md:flex">
-      <a className="btn btn-ghost btn-circle" href="#toc">
+    <div className="m-auto fixed top-24 left-5 pb-3 z-50 text-sm hidden h-3/4 flex-col xl:flex">
+      <Button variant="ghost">
         <TableOfContents size={20} />
-      </a>
+      </Button>
       <div
         className={cn(
           "flex-1 text-foreground/80",
-          "group-hover/toc:opacity-45 opacity-0 transition-transform-opacity hover:opacity-80",
-          "duration-300 ease-in-out transform-gpu group-hover/toc:translate-x-0 translate-x-5",
+          "group-hover/toc:opacity-45 opacity-0 transition-transform-opacity",
+          "duration-300 ease-in-out transform-gpu group-hover/toc:translate-x-15 translate-x-5",
           "overflow-y-auto h-full pb-10 left-0",
         )}
       >
